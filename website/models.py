70963e3184ebc00b660e62eb22438f149fe32c89
BrendaSigobia <sigobiabrenda@gmail.com>
2023-11-01 23:40:07 -0300

    commit ac3

--- website/models.py
+++ website/models.py
@@ -1,5 +1,6 @@
 from . import db
 from flask_login import UserMixin
+from sqlalchemy.sql import func
 
 
 class Usuario(db.Model, UserMixin):
@@ -7,3 +8,11 @@ class Usuario(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     primeiro_nome = db.Column(db.String(150))
+    notas = db.relationship('Nota')
+
+
+class Nota(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(50000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    id_usuario = db.Column(db.Integer, db.ForeignKey('usuario.id'))
--- website/views.py
+++ website/views.py
@@ -1,13 +1,26 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
+from .models import Nota
 from flask_login import login_required, current_user
+from . import db
 
 
 views = Blueprint('views', __name__)
 
 
-@views.route('/minhas_notas')
+@views.route('/minhas_notas',  methods=['GET', 'POST'])
 @login_required
 def minhas_notas():
+    if request.method == 'POST':
+        nota = str(request.form.get('nota'))
+
+        if len(nota) < 1:
+            flash('Preencha sua anotação!', category='Erro')
+        else:
+            nova_nota = Nota(data=nota, id_usuario=current_user.id)
+            db.session.add(nova_nota)
+            db.session.commit()
+            flash('Anotação salva!', category='mensagem_de_sucesso')
+
     return render_template("notas.html", usuario=current_user)
 
 
